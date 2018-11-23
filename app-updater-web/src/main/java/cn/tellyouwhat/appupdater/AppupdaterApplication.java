729679c0fdc4881260a3f792f29d717f704e4bfe
ZengXG <s_zengxiaogang@wps.cn>
2018-11-23 14:18:01 +0800

    init commit

---
+++ app-updater-web/src/main/java/cn/tellyouwhat/appupdater/AppupdaterApplication.java
@@ -0,0 +1,12 @@
+package cn.tellyouwhat.appupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppupdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppupdaterApplication.class, args);
+    }
+}
---
+++ app-updater-web/src/test/java/cn/tellyouwhat/appupdater/AppupdaterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.tellyouwhat.appupdater;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppupdaterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
